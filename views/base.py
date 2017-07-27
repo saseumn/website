e7319ee7210c7d6d6ee6d820dcebab17825ded2a
Michael Zhang <failed.down@gmail.com>
2017-07-26 21:03:34 -0500

    Deployment check.

--- views/base.py
+++ views/base.py
@@ -6,3 +6,7 @@ blueprint = Blueprint("base", __name__)
 @blueprint.route("/")
 def index():
     return render_template("base/index.j2")
+
+@blueprint.route("/sanity")
+def sanity():
+    return "im sane"
